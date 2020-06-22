6e1ffcc8a698a770b237409c8cd3633bede7e462
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-06-22 10:31:39 +0300

    reorder tasks with dnd

--- src/column.jsx
+++ src/column.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+import Task from "./task";
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgray;
   border-radius: 2px;
 `;
-const Title = styled.h3``;
-const TaskList = styled.div``;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
 
 class Column extends React.Component {
   render() {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <TaskList>tasks go here</TaskList>
+        <Droppable droppableId={this.props.column.id}>
+          {provided => (
+            <TaskList
+              ref={provided.innerRef}
+              innerRef={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {this.props.tasks.map((task, index) => (
+                <Task key={index} task={task} index={index} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
       </Container>
     );
   }
